64c880b111ff6732ee5a5e6593db120109590e37
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-12 16:47:55 +0700

    Hoan tat

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //Mẫu
+//Them
+
+const scorers = {
+  Gnarby: 1,
+  Hummels: 1,
+  Lewandowski: 2,
+};
 
 const game = {
   team1: "Bayern Munich",
@@ -39,8 +46,10 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
+  scorers,
 };
 
+console.log(game);
 //Lab1.1.1
 
 const [player1, player2] = game.players;
@@ -79,3 +88,23 @@ printGoals(...game.scored);
 //Lab1.1.7
 
 game.odds.team1 < game.odds.team2 && console.log("Team 1 dành chiến thắng!");
+
+//Lab1.2.1
+
+for (const [i, ghiban] of game.scored.entries()) {
+  console.log(`${i + 1}: Số bàn thắng: 1-${ghiban}`);
+}
+
+//Lab1.2.2
+const odds = Object.values(game.odds);
+let avg = 0;
+for (const odd of odds) avg += odd;
+avg /= odds.length;
+console.log(avg);
+
+//Lab1.2.3
+
+for (const [team, tyle] of Object.entries(game.odds)) {
+  const nameTeam = team === "x" ? "Hòa" : `Thắng ${game[team]}`;
+  console.log(`Ty le ${nameTeam} la ${tyle} phan tram!`);
+}
