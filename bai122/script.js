10df7636f8b3ef5be55ddd6ecb10aca9ce76d3f3
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-25 16:20:59 +0700

    uuuuu

---
+++ bai122/script.js
@@ -0,0 +1,13 @@
+const checkmid = function (seat) {
+  const s = seat.slice(-1);
+  if (s == "B" || s == "E") console.log("Ban ngoi giu hang nha");
+  else console.log("Chuc ban co mot chuyen di vui ve");
+};
+
+checkmid("23B");
+checkmid("23A");
+checkmid("23E");
+
+console.log(new String("Hahahhaha"));
+
+console.log(typeof new String("haha"));
