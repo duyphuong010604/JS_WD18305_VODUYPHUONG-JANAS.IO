5784f82d643214dd624649024213f5239afee015
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-31 23:07:53 +0700

    u

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// GET request for remote image in node.js
 axios({
-  method: "get",
-  url: "http://localhost:3000/posts/1",
-  data: {
-    title: "a title",
-    views: 100,
-  },
+  method: "delete",
+  url: "http://localhost:3000/data",
 }).then(function (response) {
   console.log(response);
 });
